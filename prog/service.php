6b58e1c6cfdadebc8b08268685964da30c626ba4
chanx3 <pvchanupamal@gmail.com>
2018-11-25 10:31:17 +0530

    image change

--- prog/service.php
+++ prog/service.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  
+
 <!-- Mirrored from ekan-admin-templates.multipurposethemes.com/main/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 20 Nov 2018 01:08:32 GMT -->
 <head>
     <meta charset="utf-8">
@@ -8,29 +8,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="http://ekan-admin-templates.multipurposethemes.com/images/favicon.ico">
+
 
     <title>AJITH MOTORS</title>
-    
+
 	<!-- Bootstrap 4.0-->
 	<link rel="stylesheet" href="assets/vendor_components/bootstrap/dist/css/bootstrap.css">
-	
-	<!-- daterange picker -->	
+
+	<!-- daterange picker -->
 	<link rel="stylesheet" href="assets/vendor_components/bootstrap-daterangepicker/daterangepicker.css">
-	
+
     <!-- c3 CSS -->
     <link rel="stylesheet" type="text/css" href="assets/vendor_components/c3/c3.min.css">
-	
+
 	<!-- Bootstrap extend-->
 	<link rel="stylesheet" href="css/bootstrap-extend.css">
-	
+
 	<!-- theme style -->
 	<link rel="stylesheet" href="css/master_style.css">
-	
+
 	<!-- Ekan Admin skins -->
 	<link rel="stylesheet" href="css/skins/_all-skins.css">
 	<link rel="stylesheet" type="text/css" href="design.css">
-	
+
 
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -39,7 +39,7 @@
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
 
-     
+
   </head>
 
 <body class="hold-transition skin-info dark-sidebar light sidebar-mini">
@@ -49,11 +49,11 @@
     <!-- Logo -->
     <a href="index-2.html" class="logo">
       <!-- mini logo -->
-	  
+
       <!-- logo-->
       <div class="logo-lg">
-		  <span class="light-logo"><img src="../images/logo-light-text.png" alt="logo"></span>
-	  	  <span class="dark-logo"><img src="../images/logo-dark-text.png" alt="logo"></span>
+		  <span class="light-logo"><img src="yamaha.jpg" alt="logo" style="width:75px ;height:75px"></span>
+
 	  </div>
     </a>
     <!-- Header Navbar -->
@@ -71,27 +71,27 @@
 				<button type="button" class="btn  btn-default" data-target="#search_form" data-toggle="collapse" aria-label="Close" aria-expanded="true"><i class="mdi mdi-magnify"></i></button>
 				</span>
 			</div>
-		  </form> 
-	  </div>     
+		  </form>
+	  </div>
     </nav>
   </header>
-  
+
   <!-- Left side column. contains the logo and sidebar -->
   <aside class="main-sidebar">
     <!-- sidebar-->
     <section class="sidebar">
-		  
+
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="image">
-          <img src="../images/logo-big.png" class="rounded-circle" alt="User Image">
+          <img src="yamaha2.jpg"  alt="User Image" style="width:250px; height:100px">
         </div>
         <div class="info">
-			
+
             <a href="#" class="link" data-toggle="tooltip" title="" data-original-title="Logout"><i class="ion ion-power"></i></a>
         </div>
       </div>
-      
+
       <!-- sidebar menu-->
       <ul class="sidebar-menu" data-widget="tree">
 		<div class="sideup">
@@ -104,26 +104,26 @@
           </a>
          </li>
           </ul>
-        </li> 
+        </li>
         <div class="side">
 		  <br>
 			<li class="header nav-small-cap">MAIN(WEB)</li> <br>
-			   
+
 	        <li class="header nav-small-cap">SERVICE CENTER</li><br>
-			  
-	        <li class="header nav-small-cap">SHOWROOM</li><br>    
-			
+
+	        <li class="header nav-small-cap">SHOWROOM</li><br>
+
 	        <li class="header nav-small-cap">CONTACT</li><br>
-		  
-			<li class="header nav-small-cap">MAKE AN ADMIN</li><br>	
-		</div>		  	
-    </section>  
+
+			<li class="header nav-small-cap">MAKE AN ADMIN</li><br>
+		</div>
+    </section>
   </aside>
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
 	  <div class="container-full">
-		<!-- Content Header (Page header) -->	  
+		<!-- Content Header (Page header) -->
 		<div class="content-header">
 			<div class="d-flex align-items-center">
 				<div class="mr-auto">
@@ -137,40 +137,40 @@
 						</nav>
 					</div>
 				</div>
-				
+
 <!-- ./wrapper -->
-  	
-	 
-	  
+
+
+
 	<!-- jQuery 3 -->
 	<script src="assets/vendor_components/jquery-3.3.1/jquery-3.3.1.js"></script>
-	
+
 	<!-- jQuery UI 1.11.4 -->
 	<script src="assets/vendor_components/jquery-ui/jquery-ui.js"></script>
-	
+
 	<!-- popper -->
 	<script src="assets/vendor_components/popper/dist/popper.min.js"></script>
-	
+
 	<!-- Bootstrap 4.0-->
-	<script src="assets/vendor_components/bootstrap/dist/js/bootstrap.js"></script>	
-	
+	<script src="assets/vendor_components/bootstrap/dist/js/bootstrap.js"></script>
+
 	<!-- date-range-picker -->
 	<script src="assets/vendor_components/moment/min/moment.min.js"></script>
 	<script src="assets/vendor_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-	
-	
-	
+
+
+
 
 	<script src="js/template.js"></script>
-	
+
 
 	<script src="js/pages/dashboard.js"></script>
 	<script src="js/pages/dashboard-chart.js"></script>
-	
-	
-	<script src="js/demo.js"></script>	
-	
-	
+
+
+	<script src="js/demo.js"></script>
+
+
 </body>
 
 </html>
