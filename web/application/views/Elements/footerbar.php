f283796155d853d5184b5dafa703c9c70d763cfd
Chamith95 <chamithjay@gmail.com>
2018-11-28 03:04:05 +0530

    Added a basic footer and improved showroom components

---
+++ web/application/views/Elements/footerbar.php
@@ -0,0 +1,49 @@
+<style>     
+       #footer{
+           background-color:darkgrey;
+           padding-top: 50px;
+           margin-top: 50px;
+           text-align: center;
+           position: relative;
+            bottom: -50px;
+           width: 100%;
+           
+       }
+    
+    #links{
+        font-size: 18px;
+        
+    }
+    
+    #links a{
+    display: inline;
+    margin: 10px;
+    font-size: 18px;
+}
+    #footer .footer-copyright {
+    padding-top: 3px;
+    padding-bottom: 3px;
+    text-align: center;
+}    
+      </style>
+
+<div  id="footer">
+        <div class="row">
+          <div class="col-xl">
+           
+           <h2>Ajith Motors</h2>
+           <p class="lead">we provide all your bike needs</p>
+            <div class="container" id=links>
+            <ul>
+                <a href="#">Company Information</a>
+                <a href="#">Contact us</a>
+                <a href="#">Reviews</a>
+                <a href="#">Terms of service</a>
+            </ul>
+       
+        </div>
+            
+        </div>
+        </div>
+         <p class="footer-copyright">© 2018 Copyright Text</p>
+    </div>
--- web/application/views/showroomMain.php
+++ web/application/views/showroomMain.php
@@ -1,12 +1,13 @@
 <?php include('Elements/header.php')?>
 
-</nav>
-    <h1>Hello, world!</h1>
 
+ <div class=p-4>
+   <h1>New Arrivals</h1>
+</div>
 <!--   Card items-->
 <div class="card-deck m-2">
   <div class="card">
-    <img class="card-img-top" src="images/wol1.jpg" alt="Card image cap">
+    <img class="card-img-top" src=https://www.setaswall.com/wp-content/uploads/2017/06/Honda-Bike-Wallpapers-12-3200-x-2400-768x576.jpg alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -17,26 +18,32 @@
 
     </div>
   </div>
-  <div class="card">
-    <img class="card-img-top" src="images/wol1.jpg" alt="Card image cap">
+    <div class="card">
+    <img class="card-img-top" src=https://www.setaswall.com/wp-content/uploads/2017/06/Honda-Bike-Wallpapers-12-3200-x-2400-768x576.jpg alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
     </div>
     <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
+     <a href="#" class="btn btn-primary">Go somewhere</a>
+     <div class="float-right"><small class="text-muted">Last updated 3 mins ago</small></div><br>
+
     </div>
   </div>
-  <div class="card">
-    <img class="card-img-top" src="images/wol1.jpg" alt="Card image cap">
+    <div class="card">
+    <img class="card-img-top" src=https://www.setaswall.com/wp-content/uploads/2017/06/Honda-Bike-Wallpapers-12-3200-x-2400-768x576.jpg alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
     </div>
     <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
+     <a href="#" class="btn btn-primary">Go somewhere</a>
+     <div class="float-right"><small class="text-muted">Last updated 3 mins ago</small></div><br>
+
+    </div>
     </div>
   </div>
-</div>
+  
+
 
 <?php include('Elements/footer.php')?>
