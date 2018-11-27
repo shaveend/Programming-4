4b646be932911942496443a0d3a5abff7e94e5a6
thiranja <lakrandikathiranja@gmail.com>
2018-11-27 16:04:37 +0530

    inizial commit with user interfaces

---
+++ web/application/views/welcome.php
@@ -0,0 +1,46 @@
+<?php include('Elements/header.php')?>
+
+
+
+	<!-- Welcome page corosal  -->
+
+	<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="moterbike_all.jpg" alt="First slide">
+			<div class="carousel-caption d-none d-md-block">
+		    <h5>Ajith Motors</h5>
+		    <p>Moterbike Solution</p>
+		  </div>
+		</div>
+		<div class="carousel-item">
+		  <img class="d-block w-100" src="Images/moterbike_showcase.jpg" alt="...">
+		  <div class="carousel-caption d-none d-md-block">
+		    <h5>Ajith Showcase</h5>
+		    <p>Dream Wheels come true here</p>
+		  </div>
+		</div>
+
+		<div class="carousel-item">
+		  <img class="d-block w-100" src="Images/moterbike_service.jpg" alt="...">
+		  <div class="carousel-caption d-none d-md-block">
+		    <h5>Ajith Services</h5>
+		    <p>You ride it, We take care of it!</p>
+		  </div>
+		</div>
+
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<!-- Corosal ends here -->
+<img src="Images/moterbike_showcase.jpg" alt="...">
+
+<?php include('Elements/footer.php')?>
