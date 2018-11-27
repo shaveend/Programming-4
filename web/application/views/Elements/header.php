3ac61e3480b7814b807b2ae0f4512aedd5e70b21
thiranja <lakrandikathiranja@gmail.com>
2018-11-28 00:55:45 +0530

    showroom and service pages added to the system

--- web/application/views/Elements/header.php
+++ web/application/views/Elements/header.php
@@ -29,17 +29,17 @@
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Showroom</a>
+        <a class="nav-link" href="<?php echo base_url('index.php/Showcase');?>">Showroom</a>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" href="<?php echo base_url('index.php/Service');?>" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Service Center
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="<?php echo base_url('index.php/Service');?>">Action</a>
+          <a class="dropdown-item" href="<?php echo base_url('index.php/Service');?>">Another action</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="<?php echo base_url('index.php/Service');?>">Something else here</a>
         </div>
       </li>
       <li class="nav-item">
---
+++ web/application/views/service.php
@@ -0,0 +1,3 @@
+<?php include('Elements/header.php')?>
+<h1>Service page is this</h1>
+<?php include('Elements/footer.php')?>
