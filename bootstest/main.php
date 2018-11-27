58f22a5063e7060749c269323496290a15095409
Chamith Jayathilaka <chamithjay@gmail.com>
2018-11-27 16:22:28 +0530

    Add files via upload

--- bootstest/main.php
+++ bootstest/main.php
@@ -15,7 +15,13 @@
   <body>
 <!--  navbar-->
   <?php include"navbar.php" ?>
-    <h1>Hello, world!</h1>
+<div class="jumbotron" style="background-image: url(images/back.jpg); background-size: 100% 100%;">
+  <h1 class="display-4">Ajith Motors!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Sign up</a>
+</div>
 
 <!--   Card items-->
 <div class="card-deck m-2">
@@ -38,20 +44,39 @@
       <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
     </div>
     <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
+     <a href="#" class="btn btn-primary">Go somewhere</a>
+     <div class="float-right"><small class="text-muted">Last updated 3 mins ago</small></div><br>
+
     </div>
   </div>
   <div class="card">
     <img class="card-img-top" src="images/wol1.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
     </div>
     <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
+     <a href="#" class="btn btn-primary">Go somewhere</a>
+     <div class="float-right"><small class="text-muted">Last updated 3 mins ago</small></div><br>
     </div>
   </div>
+    <div class="card">
+    <img class="card-img-top" src="images/wol1.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+    </div>
+    <div class="card-footer">
+     <a href="#" class="btn btn-primary">Go somewhere</a>
+     <div class="float-right"><small class="text-muted">Last updated 3 mins ago</small></div><br>
+
+    </div>
+  </div>
+  
 </div>
+ 
+  
+   
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
