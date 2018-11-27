323dc2b0dfbb25c946e7d054a74f5d84b970b53b
thiranja <lakrandikathiranja@gmail.com>
2018-11-27 16:51:16 +0530

    Added hila's componants to project

--- web/application/views/Elements/header.php
+++ web/application/views/Elements/header.php
@@ -16,8 +16,8 @@
 
 <body>
 
-	<!-- Navbar Co[de comes Here -->
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<!-- Navbar Code comes Here -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -46,19 +46,9 @@
         <a class="nav-link disabled" href="#">Disabled</a>
       </li>
     </ul>
-
-    <!-- <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form> -->
-    <ul>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Login</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Register</a>
-      </li>
-  </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <a class="btn btn-outline-success my-2 my-sm-0" href="#" role="button">Login</a>
+    </form>
   </div>
 </nav>
 <!-- Navbar code ends here -->
